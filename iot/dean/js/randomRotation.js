f9140aa9546ef0bfa5b8fbd68c3911efabcc87f7
Charles Brandt <luftbuefel@users.noreply.github.com>
2025-04-09 00:59:20 -0700

    new build v1

--- iot/dean/js/randomRotation.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent("random-rotate-z", {
-    init:function(){
-        let zRotation = Math.random()*360;
-        this.el.setAttribute("rotation", `0 0 ${zRotation}`);
-    },
-    remove:function(){        
-    }
-});
---
+++ p5/gameOfLife.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Processing from '../Processing.js';
+
+const GameOfLife = function(props){
+  return(
+    <div>      
+      <script src="https://cdn.jsdelivr.net/npm/p5@1.11.3/lib/p5.min.js"></script>
+    </div>
+  );
+}
+
+export default GameOfLife;
